0ac883bca57f56660f2b19ee58bc0237645034d6
jesus2801 <jesusdavid280147@gmail.com>
2021-06-25 13:32:02 -0500

    apollo client bug fixed

--- client/src/config/apollo.config.ts
+++ client/src/config/apollo.config.ts
@@ -4,7 +4,17 @@ const client = new ApolloClient({
   link: new HttpLink({
     uri: 'http://localhost:3004/graphql',
   }),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          getWords: {
+            merge: (existing, incoming) => incoming,
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
